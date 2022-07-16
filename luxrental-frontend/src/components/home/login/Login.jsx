b4103ecab04ef7d38cf392ffa6182588ab79675d
Rashmi Pituwala <83907599+rashmiICT@users.noreply.github.com>
2022-07-16 10:37:18 +0530

    Adding login  page

---
+++ luxrental-frontend/src/components/home/login/Login.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { useMutation } from "react-query";
+import axios from "axios";
+import "./login.css";
+import { getProfile, setProfile } from "../../../utils/user";
+import { useHistory } from "react-router-dom";
+
+export default function Login({ setUser }) {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  useEffect(() => {
+    getProfile("user") && history.push("/");
+  });
+
+  const loginRequest = async (data) => {
+    return await axios.post("http://localhost:4000/users/authenticate", {
+      username: data.userName,
+      password: data.password,
+    });
+  };
+
+  const onSuccess = (data) => {
+    setProfile(data.data);
+    setUser(getProfile());
+    history.push("/");
+  };
+  const onError = (error) => {
+    alert(error.response.data.message);
+  };
+
+  const mutation = useMutation(loginRequest, {
+    onError,
+    onSuccess,
+  });
+
+  const login = () => {
+    // validate login
+    if (!userName) {
+      alert("User Name Required");
+    }
+    if (!password) {
+      alert("Password Required");
+    }
+    mutation.mutate({ userName, password });
+  };
+
+  return (
+    <div className="login">
+      <form className="loginForm">
+        <span className="loginTitle">Sign In</span>
+        <label>User Name</label>
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="Enter Your User Name"
+          value={userName}
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="loginInput"
+          placeholder="Enter Your Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button
+          className="loginButton"
+          onClick={(e) => {
+            e.preventDefault();
+            login();
+          }}
+        >
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
+}
