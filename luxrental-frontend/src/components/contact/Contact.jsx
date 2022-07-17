2fc5279a321d0e55103940a1c2deee1ec2098173
Rashmi Pituwala <83907599+rashmiICT@users.noreply.github.com>
2022-07-18 00:14:59 +0530

    Adding dialog box

--- luxrental-frontend/src/components/contact/Contact.jsx
+++ luxrental-frontend/src/components/contact/Contact.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import Back from '../common/Back'
 import img from "../images/pricing1.jpg"
 import "./contact.css"
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions
+} from '@mui/material'
+import { useState } from 'react'
 
 const Contact = () => {
+  const [open, setOpen] = useState(false)
   return (
     <>
       <section className='contact mb'>
@@ -17,7 +27,24 @@ const Contact = () => {
             </div>
             <input type='text' placeholder='Subject' />
             <textarea cols='30' rows='10'></textarea>
-            <button>Submit Request</button>
+            <button onClick={() => setOpen(true)}>Submit Request</button>
+            <Dialog
+                    open={open}
+                    onClose={() => setOpen(false)}
+                    aria-labelledby='dialog-title'
+                    aria-describedby='dialog-description'>
+                    <DialogTitle id='dialog-title'>Thank you Contact our Company</DialogTitle>
+                    <DialogContent>
+                      <DialogContentText id='dialog-description'>
+                        Are you sure you want to submit the test? You will not be able to
+                        edit it after submitting.
+                      </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                      <Button onClick={() => setOpen(false)}>OK</Button>
+              
+                    </DialogActions>
+                  </Dialog> 
           </form>
         </div>
       </section>
