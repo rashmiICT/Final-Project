e51976d0216818e76a22492996c3385dd1ef6fe6
Rashmi Pituwala <83907599+rashmiICT@users.noreply.github.com>
2022-07-18 00:08:53 +0530

    Adding Massege box

--- luxrental-frontend/src/components/home/Booking/Booking.js
+++ luxrental-frontend/src/components/home/Booking/Booking.js
@@ -4,11 +4,19 @@ import book01 from '../../images/images/book1.jpg'
 import book02 from '../../images/images/book2.jpg'
 import Back from '../../common/Back';
 import img from "../../images/images/book2.jpg"
-
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions
+} from '@mui/material'
+import { useState } from 'react'
 
 
 function Booking() {
-
+  const [open, setOpen] = useState(false)
   return (
     <section className='contact mb'>
       <Back name='Book Your Car' title='Compare Low Rates from Top Brands' cover={img} />
@@ -28,7 +36,24 @@ function Booking() {
             <input type='text' placeholder='Location'/>
             <input type='text' placeholder='Car Type'/>
 
-            <button className='loginButton'>Book Now</button>
+            <button className='loginButton' onClick={() => setOpen(true)}>Book Now</button>
+            <Dialog
+                    open={open}
+                    onClose={() => setOpen(false)}
+                    aria-labelledby='dialog-title'
+                    aria-describedby='dialog-description'>
+                    <DialogTitle id='dialog-title'>Thank you ,Your car is booked</DialogTitle>
+                    <DialogContent>
+                      <DialogContentText id='dialog-description'>
+                        Are you sure you want to submit the test? You will not be able to
+                        edit it after submitting.
+                      </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                      <Button onClick={() => setOpen(false)}>OK</Button>
+              
+                    </DialogActions>
+                  </Dialog> 
 
           </form>
         </div>
