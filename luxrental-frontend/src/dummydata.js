da39892199c58bb1b01a6d2616b25791dd12b9bc
Rashmi Pituwala <83907599+rashmiICT@users.noreply.github.com>
2022-07-18 13:50:50 +0530

    change dummy file

--- luxrental-frontend/src/dummydata.js
+++ luxrental-frontend/src/dummydata.js
@@ -2,7 +2,7 @@ export const homeAbout = [
   {
     id: 1,
     cover: "https://cdn-icons-png.flaticon.com/128/4793/4793486.png",
-    title: "Save on your next trip",
+    title: "save",
     desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
   },
   {
